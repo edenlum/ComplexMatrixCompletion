552a8bec017b78a71256dfe138993606e6538d8e
Edo Cohen-Karlik <edocoh@google.com>
2023-12-23 21:55:34 +0200

    adding support for logging to csv files.

--- pytorch/matrix_completion.py
+++ pytorch/matrix_completion.py
@@ -3,24 +3,31 @@ import torch
 import wandb
 import numpy as np
 import itertools
+from collections import defaultdict
+import pandas as pd
 
 from matrix_completion_utils import MatrixMultiplier, Data, effective_rank
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def train(init_scale, complex_init_scale, step_size, mode, n_train, n, rank, depth, epochs=10001, smart_init=True):
-    mm = MatrixMultiplier(depth, n, mode, init_scale, complex_init_scale, smart_init)
-    dataObj = Data(n=n, rank=rank)
+def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth, epochs=20001, smart_init=True, use_wandb=True, seed=1):
+    mm = MatrixMultiplier(depth, n, mode, init_scale, diag_init_scale, smart_init)
+    dataObj = Data(n=n, rank=rank, seed=seed)
     observations_gt, indices = dataObj.generate_observations(n_train)
     
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
     mm.to(device)
     observations_gt = observations_gt.to(device)
 
     criterion = nn.MSELoss()
+    # optimizer = optim.SGD(mm.parameters(), lr=step_size)
     optimizer = optim.SGD(mm.parameters(), lr=step_size)
 
     singular_values_list = []
     balanced_diff_list = []
+    df_dict = defaultdict(list)
     for epoch in range(epochs):
         optimizer.zero_grad()
 
@@ -34,43 +41,45 @@ def train(init_scale, complex_init_scale, step_size, mode, n_train, n, rank, dep
         train_loss.backward()
         optimizer.step()
         if epoch % 10 == 0:
-            _, S, _ = torch.svd(pred)
-            singular_values_list.append(S.tolist())
-            balanced_diff_list.append(mm.calc_balanced())
             eff_rank = effective_rank(pred)
+            if use_wandb:
+                _, S, _ = torch.svd(pred)
+                singular_values_list.append(S.tolist())
+                balanced_diff_list.append(mm.calc_balanced())
 
-            # # for debugging 2 layers only
-            # real_parts = mm.calc_real_parts()
-            # eff_rank_parts = [effective_rank(p) for p in real_parts]
-            wandb.log({
-              "epoch": epoch, 
-              "train_loss": train_loss, 
-              "val_loss": val_loss, 
-              "effective_rank": eff_rank,
-            #   "effective_rank_ac": eff_rank_parts[0],
-            #   "effective_rank_bd": eff_rank_parts[1]
-            })
+                wandb.log({
+                "epoch": epoch, 
+                "train_loss": train_loss, 
+                "val_loss": val_loss, 
+                "effective_rank": eff_rank,
+                })
+            else:
+                # log all relevant variable.
+                for var in ['train_loss', 'val_loss']:
+                    df_dict[var].append(eval(var).item())
+                df_dict['eff_rank'].append(eff_rank)
 
-        if epoch % 1000 == 0:
+        if epoch % 100 == 0:
             print(f'Epoch {epoch}/{epochs}, Train Loss: {train_loss.item():.2f}, Val Loss: {val_loss.item():.2f}')
 
+    if use_wandb:
+        wandb.log({
+            "singular_values" : wandb.plot.line_series(
+                        xs=list(range(epochs//10)), 
+                        ys=list(zip(*singular_values_list)),
+                        title="Singular Values",
+                        xname="epoch/10"
+            ),
+            "balanced_diff" : wandb.plot.line_series(
+                        xs=list(range(epochs//10)),
+                        ys=list(zip(*balanced_diff_list)),
+                        title="Balanced Difference",
+                        xname="epoch/10"
+        )})
     
-    wandb.log({
-        "singular_values" : wandb.plot.line_series(
-                      xs=list(range(epochs//10)), 
-                      ys=list(zip(*singular_values_list)),
-                      title="Singular Values",
-                      xname="epoch/10"
-        ),
-        "balanced_diff" : wandb.plot.line_series(
-                      xs=list(range(epochs//10)),
-                      ys=list(zip(*balanced_diff_list)),
-                      title="Balanced Difference",
-                      xname="epoch/10"
-    )})
-
     print("Training complete")
-
+    
+    return df_dict
 
 def experiments(kwargs):
     # Extract argument names and their corresponding value lists
@@ -83,36 +92,49 @@ def experiments(kwargs):
         yield dict(zip(arg_names, values))
 
 def main():
-    for j in range(5):
-        np.random.seed(j)
+    for j in np.arange(1):
         for i, kwargs in enumerate(experiments({
-                "init_scale":           [0.1],
-                "complex_init_scale":   [0.0],
-                "step_size":            [1e-1],
+                "init_scale":           [0.0],
+                # "diag_init_scale":      [1e-3, 5e-3, 1e-2, 5e-2, 1e-1],
+                "diag_init_scale":      [1e-1],
+                # "step_size":            [5e-2, 1e-1, 5e-1],
+                "step_size":            [5e-1],
+                # "step_size":            [5e-1],
                 "mode":                 ['real'],
-                "n_train":              [200],
-                "n":                    [20],
+                "n_train":              [2000],
+                "n":                    [100],
                 "rank":                 [5],
                 "depth":                [4],
-                "smart_init":           [False],
+                "smart_init":           [True],
+                "use_wandb":            [True],
+                "seed":                 np.arange(10),
         })):
+            if not kwargs['smart_init'] and kwargs['diag_init_scale'] > 0:
+                raise ValueError("If 'smart init' is False the 'diag_init_scale' must be set to 0.")
             print('#'*100 + f"\n{kwargs}\n" + "#"*100)
             if kwargs['smart_init'] and kwargs['mode'] == 'complex' and kwargs['init_scale'] == 0.0:
-                exp_name = "rnd_init_{}_complex_diaginit-initscale_{}_lr_{}_depth_{}".format(j, kwargs['complex_init_scale'], kwargs['step_size'], kwargs['depth'])
-            elif kwargs['mode'] == 'real' and kwargs['init_scale'] > 0.0 and kwargs['complex_init_scale'] == 0.0:
-                exp_name = "rnd_init_{}_real-initscale_{}_lr_{}_depth_{}".format(j, kwargs['init_scale'], kwargs['step_size'], kwargs['depth'])
+                exp_name = "rnd_init_{}_depth_{}_complex_diaginitscale_{}_lr_{}".format(kwargs['seed'], kwargs['depth'], kwargs['diag_init_scale'], kwargs['step_size'])
+            # elif kwargs['mode'] == 'real' and kwargs['init_scale'] > 0.0 and kwargs['diag_init_scale'] == 0.0:
+            elif kwargs['mode'] == 'real':
+                exp_name = "rnd_init_{}_depth_{}_real_diaginitscale_{}_initscale_{}_lr_{}".format(kwargs['seed'], kwargs['depth'], kwargs['diag_init_scale'], kwargs['init_scale'], kwargs['step_size'])
             else:
                 raise ValueError('Testing only real vs complex with diagonal init.')
-
-            wandb.init(
-                project="ComplexMatrixCompletion",
-                # entity="complex-team",
-                # name=f"experiment-{i}",
-                name=exp_name,
-                config=kwargs
-            )
-            train(**kwargs)
-            wandb.finish()
+            
+            if kwargs['use_wandb']:
+                wandb.init(
+                    project="ComplexMatrixCompletion",
+                    # entity="complex-team",
+                    # name=f"experiment-{i}",
+                    name=exp_name,
+                    config=kwargs
+                )
+                train(**kwargs)
+                wandb.finish()
+            else:
+                results = train(**kwargs)
+                results_df = pd.DataFrame(results)
+                curr_dir = 'diag_init' if kwargs['smart_init'] else 'standard_init'
+                results_df.to_csv('pytorch/results/{}/{}.csv'.format(curr_dir, exp_name))
 
 if __name__=='__main__':
     main()
--- pytorch/matrix_completion_utils.py
+++ pytorch/matrix_completion_utils.py
@@ -1,22 +1,130 @@
+import os
+import pandas as pd
 import numpy as np
 import torch
 from torch import nn
-import random
+from collections import defaultdict
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+def get_convergence_step(arr, threshold=3e-2):
+    indices = np.where(arr < threshold)[0]
+    # print(indices, len(indices))
+    return indices[0] if len(indices) > 0 else 2e4
 
-def start_direction(depth, size, complex_init_scale):
+def get_dataframes(directory_path):
+    # Create an empty list to store DataFrames
+    data_frames = {}
+
+    # Iterate over each file in the directory
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".csv"):
+            # Construct the full file path
+            file_path = os.path.join(directory_path, filename)
+            
+            # Load the CSV file into a DataFrame
+            df = pd.read_csv(file_path)
+            
+            # Append the DataFrame to the list
+            data_frames[filename] = df
+    
+    return data_frames
+
+def dataframe_to_matrix(results, agg_fn):
+    x_labels = sorted(results.keys())
+    y_labels = sorted(results[x_labels[0]].keys())
+    res = np.zeros((len(y_labels), len(x_labels)))
+    for i, lr in enumerate(x_labels):
+        for j, intscl in enumerate(y_labels):
+            res[j, i] = agg_fn(results[lr][intscl])
+    return x_labels, y_labels, res
+
+def visualize_results(results, agg_fn):
+    x_labels, y_labels, res = dataframe_to_matrix(results, agg_fn)
+    # Create a heatmap with custom axis labels
+    sns.heatmap(res, annot=True, cmap='viridis', fmt='.2f', linewidths=.5,
+                xticklabels=x_labels, yticklabels=y_labels)
+
+    # Display the heatmap
+    plt.show()
+
+def parse_results(directory_path, diag_init):
+    curr_dir = 'diag_init' if diag_init else 'standard_init'
+    init_scale_idx = 7 if diag_init else 9
+    dataframes = get_dataframes(os.path.join(directory_path, curr_dir))
+    results = defaultdict(lambda: defaultdict(list))
+    effective_rank_dict = defaultdict(lambda: defaultdict(list))
+
+    for k, v in dataframes.items():
+        exp_name = k.split('_')
+        seed = exp_name[2]
+        init_scale = exp_name[init_scale_idx]
+        lr = exp_name[11][:-4]
+        # print(results['{}'.format(lr)]['{}'.format(init_scale)])
+        # exit()
+        results['{}'.format(lr)]['{}'.format(init_scale)].append(get_convergence_step(v['val_loss'].to_numpy()))
+        effective_rank_dict['{}'.format(lr)]['{}'.format(init_scale)].append(v['eff_rank'].tolist()[-1])
+        # results['{}'.format(lr)]['{}'.format(init_scale)] = get_convergence_step(v['val_loss'].to_numpy())
+    
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    x_labels, y_labels, res = dataframe_to_matrix(results, np.mean)
+    # Create a heatmap with custom axis labels
+    sns.heatmap(res, annot=True, cmap='viridis', fmt='.2f', linewidths=.5,
+                xticklabels=x_labels, yticklabels=y_labels, ax=axes[0])
+    axes[0].set_title('Mean')
+
+    x_labels, y_labels, res = dataframe_to_matrix(results, np.std)
+    # Create a heatmap with custom axis labels
+    sns.heatmap(res, annot=True, cmap='viridis', fmt='.2f', linewidths=.5,
+                xticklabels=x_labels, yticklabels=y_labels, ax=axes[1])
+    axes[1].set_title('Std')
+
+    x_labels, y_labels, res = dataframe_to_matrix(effective_rank_dict, np.mean)
+    # Create a heatmap with custom axis labels
+    sns.heatmap(res, annot=True, cmap='viridis', fmt='.2f', linewidths=.5,
+                xticklabels=x_labels, yticklabels=y_labels, ax=axes[2])
+    axes[2].set_title('Effective Rank')
+
+    fig.suptitle(curr_dir)
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    # Show the plot
+    plt.savefig('{}_results.png'.format(curr_dir))
+    
+    return results
+
+# def start_direction(depth, size, complex_init_scale):
+#     real = np.cos((np.pi/2) / depth)
+#     imag = np.sin((np.pi/2) / depth)
+#     return torch.eye(size) * real * complex_init_scale, torch.eye(size) * imag * complex_init_scale
+
+def start_direction(depth, size, diag_init_scale, mode):
     real = np.cos((np.pi/2) / depth)
     imag = np.sin((np.pi/2) / depth)
-    return torch.eye(size) * real * complex_init_scale, torch.eye(size) * imag * complex_init_scale
+    diag_init_scale = calc_init_scale(depth, 1, diag_init_scale, mode)
+    if mode == complex:
+        return torch.eye(size) * real * diag_init_scale, torch.eye(size) * imag * diag_init_scale
+    else:
+        return torch.eye(size) * diag_init_scale, 0
+
+def calc_init_scale(depth, n, e2e_scale, mode):
+    # end to end scale = init_scale ^ depth * sqrt(n) ^ (depth - 1)
+    # if complex, then there is another sqrt(2) for every multiplication (i.e sqrt(2)^(depth-1))
+    n = n if mode == "real" else 2*n
+    init_scale = (e2e_scale / ((n**(1/2)) ** (depth - 1)))**(1/depth)
+    return init_scale
 
 class MatrixMultiplier(nn.Module):
-    def __init__(self, depth, size, mode, init_scale, complex_init_scale, smart_init=True):
+    def __init__(self, depth, size, mode, init_scale, diag_init_scale, smart_init=True):
         super(MatrixMultiplier, self).__init__()
         self.depth = depth
         self.size = size
         self.mode = mode
-        if mode=="complex" and smart_init:
-            real, imag = start_direction(depth, size, complex_init_scale)
+        # if mode=="complex" and smart_init:
+        if smart_init:
+            real, imag = start_direction(depth, size, diag_init_scale, mode)
         else:
             real, imag = 0, 0
         self.real_matrices = nn.ParameterList([nn.Parameter(torch.randn(size, size) * init_scale + real) for _ in range(depth)])
@@ -76,11 +184,13 @@ def effective_rank(mat):
 
 
 class Data:
-    def __init__(self, n, rank, symmetric=False):
+    def __init__(self, n, rank, symmetric=False, seed=1):
         self.n = n
         self.r = rank
         self.symmetric = symmetric
         self.generate_gt_matrix()
+        torch.manual_seed(seed)
+        np.random.seed(seed)
 
     def generate_gt_matrix(self):
         U = torch.randn(self.n, self.r)
@@ -95,3 +205,15 @@ class Data:
         indices = np.random.choice(self.n*self.n, size=(n_examples,), replace=False)
         return self.w_gt, indices
     
+
+def main():
+    dataObj = Data(n=10, rank=3)
+    dataObj.generate_gt_matrix()
+    print(dataObj.generate_observations(10))
+
+if __name__=='__main__':
+    results = parse_results('/Users/edocoh/work/ComplexMatrixCompletion/pytorch/results', diag_init=True)
+    results = parse_results('/Users/edocoh/work/ComplexMatrixCompletion/pytorch/results', diag_init=False)
+    # for k, v in results.items():
+    #     print(k, v)
+    # print(results)
