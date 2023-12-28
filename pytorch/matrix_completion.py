4155e03d27952a1a866986600727a3933fa08daf
Eden Lumbroso <edenlum@gmail.com>
2023-12-28 15:41:16 +0200

    adding complex e2e singular values

--- pytorch/matrix_completion.py
+++ pytorch/matrix_completion.py
@@ -6,11 +6,11 @@ import itertools
 from collections import defaultdict
 import pandas as pd
 
-from matrix_completion_utils import MatrixMultiplier, Data, effective_rank
+from matrix_completion_utils import complex_matmul, MatrixMultiplier, Data, effective_rank
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth, epochs=20001, smart_init=True, use_wandb=True, seed=1):
+def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth, epochs=5001, smart_init=True, use_wandb=True, seed=1):
     mm = MatrixMultiplier(depth, n, mode, init_scale, diag_init_scale, smart_init)
     dataObj = Data(n=n, rank=rank, seed=seed)
     observations_gt, indices = dataObj.generate_observations(n_train)
@@ -22,11 +22,12 @@ def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth,
     observations_gt = observations_gt.to(device)
 
     wandb.watch(mm)
+    wandb.config["data_eff_rank"] = effective_rank(observations_gt)
     criterion = nn.MSELoss()
-    # optimizer = optim.SGD(mm.parameters(), lr=step_size)
     optimizer = optim.SGD(mm.parameters(), lr=step_size)
 
     singular_values_list = []
+    complex_sing_values_list = []
     balanced_diff_list = []
     df_dict = defaultdict(list)
     for epoch in range(epochs):
@@ -43,14 +44,17 @@ def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth,
         optimizer.step()
         if epoch % 10 == 0:
             _, S, _ = torch.svd(pred)
-            singular_values_list.append(S.tolist()[:10])
+            singular_values_list.append(S.tolist())
             balanced_diff_list.append(mm.calc_balanced())
             eff_rank = effective_rank(pred)
-            if use_wandb:
-                _, S, _ = torch.svd(pred)
-                singular_values_list.append(S.tolist()[:10])
-                balanced_diff_list.append(mm.calc_balanced())
 
+            w_e2e = mm.matrices[0]
+            for w in mm.matrices[1:]:
+                w_e2e = complex_matmul(w, w_e2e)
+            _, S, _ = torch.svd(w_e2e[0] + 1j*w_e2e[1])
+            complex_sing_values_list.append(S.tolist())
+
+            if use_wandb:
                 wandb.log({
                   "epoch": epoch, 
                   "train_loss": train_loss, 
@@ -83,6 +87,12 @@ def train(init_scale, diag_init_scale, step_size, mode, n_train, n, rank, depth,
                         ys=list(zip(*balanced_diff_list)),
                         title="Balanced Difference",
                         xname="epoch/10"
+            ),
+            "complex_singular_values" : wandb.plot.line_series(
+                        xs=list(range(epochs//10)), 
+                        ys=list(zip(*complex_sing_values_list)),
+                        title="Singular Values",
+                        xname="epoch/10"
         )})
     
     print("Training complete")
@@ -102,24 +112,25 @@ def experiments(kwargs):
 def main():
     for j in np.arange(1):
         for i, kwargs in enumerate(experiments({
-                "init_scale":           [0.0],
-                "diag_init_scale":      [1e-1],
-                "step_size":            [5e-1],
-                "mode":                 ['real'],
+                "init_scale":           [0],
+                "diag_init_scale":      [1e-4],
+                "step_size":            [3],
+                "mode":                 ['complex'],
                 "n_train":              [2000],
                 "n":                    [100],
-                "rank":                 [5],
+                "rank":                 [50],
                 "depth":                [4],
                 "smart_init":           [True],
                 "use_wandb":            [True],
-                "seed":                 np.arange(10),
+                "seed":                 np.arange(1),
         })):
             if not kwargs['smart_init'] and kwargs['diag_init_scale'] > 0:
                 raise ValueError("If 'smart init' is False the 'diag_init_scale' must be set to 0.")
             print('#'*100 + f"\n{kwargs}\n" + "#"*100)
             if kwargs['smart_init'] and kwargs['mode'] == 'complex' and kwargs['init_scale'] == 0.0:
                 exp_name = "rnd_init_{}_depth_{}_complex_diaginitscale_{}_lr_{}".format(kwargs['seed'], kwargs['depth'], kwargs['diag_init_scale'], kwargs['step_size'])
-            # elif kwargs['mode'] == 'real' and kwargs['init_scale'] > 0.0 and kwargs['diag_init_scale'] == 0.0:
+            elif kwargs['mode'] == 'complex':
+                exp_name = "rnd_init_{}_depth_{}_complex_diaginitscale_{}_initscale_{}_lr_{}".format(kwargs['seed'], kwargs['depth'], kwargs['diag_init_scale'], kwargs['init_scale'], kwargs['step_size'])
             elif kwargs['mode'] == 'real':
                 exp_name = "rnd_init_{}_depth_{}_real_diaginitscale_{}_initscale_{}_lr_{}".format(kwargs['seed'], kwargs['depth'], kwargs['diag_init_scale'], kwargs['init_scale'], kwargs['step_size'])
             else:
@@ -128,8 +139,7 @@ def main():
             if kwargs['use_wandb']:
                 wandb.init(
                     project="ComplexMatrixCompletion",
-                    # entity="complex-team",
-                    # name=f"experiment-{i}",
+                    entity="complex-team",
                     name=exp_name,
                     config=kwargs
                 )
